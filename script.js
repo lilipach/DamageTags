7e931ef94eeae1d0951a7d20aa34c337bbf8da8d
lilipach <lilianap@nevada.unr.edu>
2021-02-04 01:43:17 -0800

    [FEAT] Form for tags is complete. Clear button functionality is complete. Still needs print button functionality/update form for printing.

---
+++ script.js
@@ -0,0 +1,43 @@
+function getDate(){
+    var date = new Date();
+
+    return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+}
+
+function runTags(){
+    //allow check box to be checked with enter
+    $('input:checkbox').keypress(function(e){
+        e.preventDefault();
+        if((e.keyCode ? e.keyCode : e.which) == 13){
+            $(this).trigger('click');
+        }
+    });
+
+    //Sign Tag
+    console.log(getDate());
+    document.getElementById("sign").innerHTML = "Name: Liliana Pacheco <br>Date:  " + getDate();           
+}
+
+function printForm(toPrint){
+    updateForm();
+    var content = document.getElementById(toPrint).innerHTML;
+    var original = document.body.innerHTML;
+
+    document.body.innerHTML = content;
+    window.print();
+    document.body.innerHTML = original;
+}
+
+function updateForm(){
+    
+}
+
+function clearForm(){
+    document.getElementById("fAssetTag").value = "";
+    document.getElementById("fDescription").value = "";
+    document.getElementById("fNextStep").value = "";
+    document.getElementById("fMemoryYes").checked = "";
+    document.getElementById("fMemoryNo").checked = "";
+    document.getElementById("fMemoryNA").checked = "";
+}
+
