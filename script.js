b21acc665683b5d75d5edad0e1bf0c021eb03cae
Pacheco <lilipach@amazon.com>
2021-04-12 18:20:02 -0700

    [FEAT] Added checkbox and radio event listeners. Added clear button functionality. Still pending print button specific to zebra printer models.

--- script.js
+++ script.js
@@ -1,51 +1,30 @@
+$(document).ready(function(){
+    $('input:checkbox, input:radio').keypress(function(e){
+        e.stopImmediatePropagation();
+        console.log(e);
+        if(e.keyCode== 13){
+            $(this).trigger('click');
+            console.log("Click")
+        }
+    });
+
+    $("#clearBtn").click(function(){
+        console.log("clear");
+        $("input:text, textarea").val("");
+        $("input:checkbox, input:radio")
+            .prop('checked', false)
+            .prop('selected', false);
+    });
+
+    console.log(getDate()); 
+    document.getElementById("sign").innerHTML = "Name: Liliana Pacheco <br>Date:  " + getDate();
+});
+
 function getDate(){
     var date = new Date();
 
     return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
 }
 
-function runTags(){
-    //allow check box to be checked with enter
-    $('input:checkbox').keypress(function(e){
-        e.preventDefault();
-        if((e.keyCode ? e.keyCode : e.which) == 13){
-            $(this).trigger('click');
-        }
-    });
 
-    
-    $('input:text').ready(function() {
-        $(window).keydown(function(event){
-          if(event.keyCode == 13) {
-            event.preventDefault();
-            return false;
-          }
-        });
-      });
-
-    //Sign Tag
-    console.log(getDate());
-    document.getElementById("sign").innerHTML = "Name: Liliana Pacheco <br>Date:  " + getDate();           
-}
-
-function printForm(toPrint){
-    console.log(document.getElementById("fAssetTag").value);
-
-    var content = document.getElementById(toPrint).innerHTML;
-    var original = document.body.innerHTML;
-
-   // document.body.innerHTML = content;
-    window.print();
-    document.body.innerHTML = original;
-    runTags();
-}
-
-function clearForm(){
-    document.getElementById("fAssetTag").value = "";
-    document.getElementById("fDescription").value = "";
-    document.getElementById("fNextStep").value = "";
-    document.getElementById("fMemoryYes").checked = "";
-    document.getElementById("fMemoryNo").checked = "";
-    document.getElementById("fMemoryNA").checked = "";
-}
 
